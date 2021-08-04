40459e78c97290565e01ef59a6d60ddfe7f1f7c4
Ria Dhanani <ria08.dhanani@gmail.com>
2021-08-04 17:21:57 +0530

    display github repositories for a selected language

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import './components/Popular.js'
+import Popular from './components/Popular.js'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Popular />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
